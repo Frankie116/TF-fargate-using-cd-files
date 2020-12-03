d714f933ab7f1c4717e3909727f42931a33e54b4
Frankie116 <frankbotchey@hotmail.com>
2020-12-03 00:48:01 +0000

    added template files

--- ecs.tf
+++ ecs.tf
@@ -41,7 +41,7 @@ resource "aws_ecs_service" "my-service" {
 
   load_balancer {
     target_group_arn = aws_alb_target_group.my-tg.id
-    container_name   = "my-container"
+    container_name   = "myapp"
     container_port   = var.app_port
   }
 
