1464c3a44c7ffb0930dd56bbe60c8e3c1a7d3b6e
Frankie116 <frankbotchey@hotmail.com>
2020-12-14 16:15:15 +0000

    added more tags to 01a-vpc.tf

--- 01a-vpc.tf
+++ 01a-vpc.tf
@@ -14,36 +14,40 @@
 
 
 module "my-vpc" {
-  source                 = "terraform-aws-modules/vpc/aws"
-  version                = "2.44.0"
-  cidr                   = var.my-vpc-cidr-block
-  azs                    = data.aws_availability_zones.my-available-azs.names
-  private_subnets        = slice(var.my-priv-subnet-cidr-blocks, 0, var.my-priv-subnets-per-vpc)
-  public_subnets         = slice(var.my-pub-subnet-cidr-blocks, 0, var.my-pub-subnets-per-vpc)
-  enable_nat_gateway     = true
-  enable_vpn_gateway     = false
-
-  tags                   = {
-    Name                 = "my-vpc-${random_string.my-random-string.result}"
-    Project              = var.my-project-name
-    Environment          = var.my-environment
-    Terraform            = "true"
+  source                   = "terraform-aws-modules/vpc/aws"
+  version                  = "2.44.0"
+  cidr                     = var.my-vpc-cidr-block
+  azs                      = data.aws_availability_zones.my-available-azs.names
+  private_subnets          = slice(var.my-priv-subnet-cidr-blocks, 0, var.my-priv-subnets-per-vpc)
+  public_subnets           = slice(var.my-pub-subnet-cidr-blocks, 0, var.my-pub-subnets-per-vpc)
+  enable_nat_gateway       = true
+  enable_vpn_gateway       = false
+  tags                     = {
+    Name                   = "my-vpc-${random_string.my-random-string.result}"
+    Project                = var.my-project-name
+    Environment            = var.my-environment
+    Terraform              = "true"
   }
-
-  private_subnet_tags    = {
-    Name                 = "my-priv-subnet-${random_string.my-random-string.result}"
+  private_subnet_tags      = {
+    Name                   = "my-priv-subnet-${random_string.my-random-string.result}"
   }
-
-  public_subnet_tags     = {
-    Name                 = "my-pub-subnet-${random_string.my-random-string.result}"
+  public_subnet_tags       = {
+    Name                   = "my-pub-subnet-${random_string.my-random-string.result}"
   }
-
-  nat_gateway_tags    = {
-    Name                 = "my-ngw-${random_string.my-random-string.result}"
+  nat_gateway_tags         = {
+    Name                   = "my-ngw-${random_string.my-random-string.result}"
   }
-  
-  igw_tags               = {
-    Name                 = "my-igw-${random_string.my-random-string.result}"
+  igw_tags                 = {
+    Name                   = "my-igw-${random_string.my-random-string.result}"
+  }
+  nat_eip_tags             = {
+    Name                   = "my-nat-eip-${random_string.my-random-string.result}"
+  }
+  private_route_table_tags = {
+    Name                   = "my-priv-rt-${random_string.my-random-string.result}"
+  }
+  public_route_table_tags  = {
+    Name                   = "my-pub-rt-${random_string.my-random-string.result}"
   }
 }
 
